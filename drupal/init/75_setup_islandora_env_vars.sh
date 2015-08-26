eea823288b5ed2c70103947d13e6ff0ef408ebee
Jacob Sanford <jsanford@unb.ca>
2015-08-26 16:45:40 -0300

    Always update solr and fedora info, since it can change

--- drupal/init/75_setup_islandora_env_vars.sh
+++ drupal/init/75_setup_islandora_env_vars.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ ! -f /tmp/DRUPAL_DB_LIVE && ! -f /tmp/DRUPAL_FILES_LIVE ]];
-then
-
 FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR="${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR:-$(echo $FEDORA_SERVER_IP)}"
 FEDORASOLRGSEARCH_PORT_8080_TCP_PORT="${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT:-$(echo $FEDORA_SERVER_PORT)}"
 
@@ -11,5 +8,3 @@ cd ${DRUPAL_ROOT}
 # Set Drupal environment variables required by Islandora modules
 drush vset islandora_base_url http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/fedora
 drush vset islandora_solr_url http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/solr/collection1
-
-fi
