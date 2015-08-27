fa98864e84f195d445056cd81404c01a829db019
Jacob Sanford <jsanford@unb.ca>
2015-08-27 14:29:46 -0300

    Be gentle with docker processes

--- refresh.sh
+++ refresh.sh
@@ -4,7 +4,7 @@
 docker pull mysql:5.6
 docker pull unblibraries/fedora-solr-gsearch:3.8.x
 docker pull unblibraries/drupal:apache
-docker-compose kill
+docker-compose stop
 docker-compose rm
 docker-compose build
 docker-compose up
