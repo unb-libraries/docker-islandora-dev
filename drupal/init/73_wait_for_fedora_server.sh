98d0cef0210c05580956ea08ef922959638c0b39
Jacob Sanford <jsanford@unb.ca>
2015-09-02 20:55:58 -0300

    Only wait for Fedora on initial install

--- drupal/init/73_wait_for_fedora_server.sh
+++ drupal/init/73_wait_for_fedora_server.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# Here, we default to using the address from a linked container named 'fedorasolrgsearch', falling back to using the
-# details provided through FEDORA_SERVER_IP and FEDORA_SERVER_PORT environment variables.
-FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR="${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR:-$(echo $FEDORA_SERVER_IP)}"
-FEDORASOLRGSEARCH_PORT_8080_TCP_PORT="${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT:-$(echo $FEDORA_SERVER_PORT)}"
+# Is this an initial deployment/build?
+if [[ ! -f /tmp/DRUPAL_DB_LIVE && ! -f /tmp/DRUPAL_FILES_LIVE ]];
+then
+  # Here, we default to using the address from a linked container named 'fedorasolrgsearch', falling back to using the
+  # details provided through FEDORA_SERVER_IP and FEDORA_SERVER_PORT environment variables.
+  FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR="${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR:-$(echo $FEDORA_SERVER_IP)}"
+  FEDORASOLRGSEARCH_PORT_8080_TCP_PORT="${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT:-$(echo $FEDORA_SERVER_PORT)}"
 
-# Crude way to determine if Fedora is ready : test to see if returns data on the /describe page as expected.
-FEDORA_ALIVE_STRING="Repository Information View"
-if ! curl -s "http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/fedora/describe" | grep -q "$FEDORA_ALIVE_STRING"; then
-  while [ "$RETVAL" = "" ];
-  do
-     RETVAL=`curl -s "http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/fedora/describe" | grep -q "$FEDORA_ALIVE_STRING"`
-     echo -e "\t Waiting for Fedora server on ${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}..."
-     sleep 10
-  done
+  # Crude way to determine if Fedora is ready : test to see if returns data on the /describe page as expected.
+  FEDORA_ALIVE_STRING="Repository Information View"
+  if ! curl -s "http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/fedora/describe" | grep -q "$FEDORA_ALIVE_STRING"; then
+    while [ "$RETVAL" = "" ];
+    do
+      RETVAL=`curl -s "http://${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}/fedora/describe" | grep -q "$FEDORA_ALIVE_STRING"`
+      echo -e "\t Waiting for Fedora server on ${FEDORASOLRGSEARCH_PORT_8080_TCP_ADDR}:${FEDORASOLRGSEARCH_PORT_8080_TCP_PORT}..."
+      sleep 10
+    done
+  fi
 fi
